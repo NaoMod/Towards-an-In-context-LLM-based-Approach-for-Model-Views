b0901b048b75d68c87f63275c9a43ee786180ccd
James Miranda <jameswpm@gmail.com>
2024-02-14 16:33:05 +0100

    Refactor app.py to remove unused code and update function parameters

--- llm-app/app.py
+++ llm-app/app.py
@@ -10,49 +10,11 @@ from langchain.text_splitter import CharacterTextSplitter
 import os
 from langchain.agents import AgentType, initialize_agent, Tool
 
-# Read the file and load the keys
-script_dir = os.path.dirname(__file__)
-token_file_path = os.path.join(script_dir, 'token.txt')
-
-langsmith_key = None
-open_ai_key = None
-
-with open(token_file_path, 'r') as token_file:
-    # Read the first line
-    open_ai_token_line = token_file.readline()
-    
-    if open_ai_token_line:
-        open_ai_key = open_ai_token_line.split('=')[1].strip()
-        print(f'Open AI Key  is loaded')
-
-    # Read the second line
-    langsmith_token_line = token_file.readline()
-
-    if langsmith_token_line:
-        langsmith_key = langsmith_token_line.split('=')[1].strip()
-        print(f'Langsmith Key is loaded')
-
-
-if langsmith_key is not None:
-    # Configure LangSmith
-    os.environ["LANGCHAIN_TRACING_V2"] = "true"
-    os.environ["LANGCHAIN_PROJECT"] = "APP"
-    os.environ["LANGCHAIN_ENDPOINT"] = "https://api.smith.langchain.com"
-    os.environ["LANGCHAIN_API_KEY"] = langsmith_key
+is_configured = False
 
 # Streamlit configurations
 st.set_page_config(page_title="🔗 Views helper")
 st.title('🔗 Views helper')
-
-# def generate_response(topic):
-#   llm = OpenAI(temperature=0.7, openai_api_key=key)
-#   # Prompt
-#   template = 'As an experienced data scientist and technical writer, generate an outline for a blog about {topic}.'
-#   prompt = PromptTemplate(input_variables=['topic'], template=template)
-#   prompt_query = prompt.format(topic=topic)
-#   # Run LLM model and print out response
-#   response = llm(prompt_query)
-#   return st.info(response)
 
 # Load PlantUML file
 def load_txt(input_file):
@@ -60,44 +22,11 @@ def load_txt(input_file):
     st.write(input_file)
   return input_file
 
-tools = []
 
 def generate_response(uploaded_file_1, uplodaded_file_2, query_text):
-    llm = ChatOpenAI(temperature=0, model="gpt-3.5-turbo", openai_api_key=open_ai_key)
+   
     if uploaded_file_1 is not None and uplodaded_file_2 is not None:
-        documents = [{"name": uploaded_file_1.name[:-4], "content": uploaded_file_1.read().decode()}, {"name": uplodaded_file_2.name[:-4], "content": uplodaded_file_2.read().decode()}]
-        # Split documents into chunks
-        text_splitter = CharacterTextSplitter(chunk_size=20, chunk_overlap=5)
-        texts = text_splitter.create_documents(list(d['content'] for d in documents))
-        # Select embeddings
-        embeddings = OpenAIEmbeddings(openai_api_key=open_ai_key)
-        # Create a vectorstore from documents
-        db = Chroma.from_documents(texts, embeddings)
-        # Create retriever interface
-        retriever = db.as_retriever()
         
-        # Create QA chain inside a tool
-        tools.append(
-            Tool(
-                name=documents[0]["name"],
-                description=f"useful when you want to answer questions about {documents[0]['name']}",
-                func=RetrievalQA.from_chain_type(llm=llm, chain_type='stuff', retriever=retriever),
-            )
-        )
-        tools.append(
-            Tool(
-                name=documents[1]["name"],
-                description=f"useful when you want to answer questions about {documents[1]['name']}",
-                func=RetrievalQA.from_chain_type(llm=llm, chain_type='stuff', retriever=retriever),
-            )
-        )
-
-        agent = initialize_agent(
-            agent=AgentType.OPENAI_FUNCTIONS,
-            tools=tools,
-            llm=llm,
-            verbose=True,
-        )
         response = agent.invoke(query_text)
         return response
     
